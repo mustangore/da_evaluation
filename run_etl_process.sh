beca785772ac3ba6d1d3c54dd49c72c45699358c
Sébastien Jelsch <mustangore@gmail.com>
2015-12-11 00:26:47 +0100

    Add split and compression

--- run_etl_process.sh
+++ run_etl_process.sh
@@ -33,9 +33,10 @@ LOGGING="${LOGGING:--Dlog4j.configuration=file:$ETL_PROCESS_HOME/jena-log4j.prop
 JVM_ARGS="$JVM_ARGS -Djava.io.tmpdir=\"$TMPDIR\""
 
 # Transform every file (excluding *.nt or *.ntriples) to N-Triples data
-echo "###########################################"
-echo "Transform RDF Data into N-Triples Format..."
-echo "###########################################"
+echo "#################################################"
+echo "   Transform RDF Data into N-Triples Format...   "
+echo "#################################################"
+
 ts=$(date +"%s");
 for entry in "$RDF_INPUT_PATH"/*
 do
@@ -54,33 +55,65 @@ echo $(expr $(date +"%s") - $ts)
 echo "Transformation done..."
 echo ""
 
-#echo "################################"
-#echo "Generate ntriples.tar.gz File..."
-#echo "################################"
-#ts=$(date +"%s");
-#time tar -czvf ntriples.tar.gz $RDF_OUTPUT_PATH/*.nt
-#echo $(expr $(date +"%s") - $ts)
-#echo "Done..."
-#echo ""
 
-# Move generated N-Triples files to HDFS
-echo "###############################"
-echo "Move ntriples.tar.gz to HDFS..."
-echo "###############################"
+# Split lineorder.nt ...
+echo "##############################"
+echo "   Split N-Triples Files...   "
+echo "##############################"
+ts=$(date +"%s");
+
+echo "Split lineorder_qb.nt to lineorder_qb.nt.000X by 20 000 000 lines"
+time split -l 20000000 --numeric-suffixes --suffix-length=3 "$RDF_OUTPUT_PATH/"lineorder_qb.nt lineorder_qb.nt.
+echo $(expr $(date +"%s") - $ts)
+echo "Remove Old lineorder_qb.nt file"
+rm "$RDF_OUTPUT_PATH/"lineorder_qb.nt
+echo $(expr $(date +"%s") - $ts)
+echo "Split done..."
+
+
+# Compress
+echo "#################################"
+echo "   Compress N-Triples Files...   "
+echo "#################################"
 
 ts=$(date +"%s");
-hdfs dfs -put $RDF_OUTPUT_PATH $HDFS_PATH
-#hdfs dfs -put ntriples.tar.gz $HDFS_PATH
+for entry in "$RDF_OUTPUT_PATH"/*
+do
+  if [ -f "$entry" ];then
+    full_filename=`basename $entry`
+    extension="${full_filename##*.}"
+    filename="${full_filename%.*}"
+
+    echo "Compress '$full_filename' to '$full_filename.gz'"
+    time gzip "$entry"
+  fi
+done
+echo $(expr $(date +"%s") - $ts)
+echo "Compression done..."
+
+
+# Move generated compressed N-Triples files to HDFS
+echo "#####################################"
+echo "   Move ntriples.tar.gz to HDFS...   "
+echo "#####################################"
+
+ts=$(date +"%s");
+time hdfs dfs -put $RDF_OUTPUT_PATH $HDFS_PATH
 echo $(expr $(date +"%s") - $ts)
 echo "Done..."
 echo ""
 
+
+
+
 # Execute ETL Process by Java Programm for loading MDM and creating OLAP Cube in Apache Kylin
-echo "######################"
-echo "Execute ETL Process..."
-echo "######################"
-ETL_CP="$ETL_PROCESS_HOME"'/etl_process_v1.jar'
+echo "############################"
+echo "   Execute ETL Process...   "
+echo "############################"
+ETL_CP="$ETL_PROCESS_HOME"'/etl_process_v1_4.jar'
 time java -cp "$ETL_CP" org.mustangore.etl.ETLProcess
 
 echo "Done..."
 echo ""
+
+
